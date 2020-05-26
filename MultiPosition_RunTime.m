b7388e9c0b0451a30c5b4c738ddfe7d5cbf62d67
hf <moyuejian@outlook.com>
2020-05-26 21:37:59 +0800

    update

--- MultiPosition_RunTime.m
+++ MultiPosition_RunTime.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+% Capture mulitpositions
 % HF 20200525
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -9,10 +9,14 @@ else
     mmc = initialize('C:\Users\qblab\Documents\MMConfig_Ti.cfg');
 end
 
-mmc.setProperty('TIXYDrive', 'SpeedX', 2)
-mmc.setProperty('TIXYDrive', 'SpeedY', 2)
+mmc.setProperty('TIXYDrive', 'SpeedX', 2);
+mmc.setProperty('TIXYDrive', 'SpeedY', 2);
+%mmc.setProperry('TILightPath', 'Label', '3-Right100')
+mmc.setProperty('TILightPath', 'State', '2'); % Use right camera
+mmc.setProperty('TINosePiece', 'Label', '1-Plan Apo 4x NA 0.20 Dry');
+%mmc.setProperty('TINosePiece', 'State', '0');% Choose 4X objective
+mmc.setProperty('LudlWheel', 'State', '0'); % Ludl filter
 
-%mmc.getExposure()
 EXPOSURE = 15; %10ms
 mmc.setExposure(EXPOSURE);
 
@@ -22,15 +26,13 @@ all_pos = importdata("U24-4XAPO-EB-control-12wells-0525.csv");
 pos_num = length(all_pos(1, :));
 t_len = 150; % 150 frame
 t_gap = 200; %200ms
-w = 2048;
-h = 2048;
+w = 2048; h = 2048;
 EXPOSURE_WAIT = EXPOSURE + 10;
-timestamp = sprintf('%s', datestr(now,'mmdd-HHMMSS.FFF'));
+
 time_map = zeros(pos_num, t_len);
 well = 1;
 well_map =  [1:6 12:-1:7];
 
-%for i = [5 6 ]
 for i = 1:pos_num
     disp(i);
     % Set new position and set PFS
@@ -46,28 +48,26 @@ for i = 1:pos_num
     else
         mmc.sleep(300);
     end
-    
+    % Use PFS for focus
     mmc.setProperty('TIPFSStatus', 'State', 'On');
     mmc.sleep(200);
     mmc.setProperty('TIPFSStatus', 'State', 'Off');
     mmc.setExposure(EXPOSURE);
-     mmc.clearCircularBuffer();
+    mmc.clearCircularBuffer(); % clear camera buffer
     
-    %img = zeros(w, h, t_len, 'uint16');
     fname = sprintf('D:/CBY/exp0526/well%dxy%d.tiff', well, i);
-    
-    % for t = 1:t_len
+    % Capture image time by time
     t = 1;
     while( t<=t_len )
         t_last = clock;  % clock in second
         mmc.snapImage();
-        mmc.sleep(EXPOSURE_WAIT);
+        mmc.sleep(EXPOSURE_WAIT); % wait for exposure
         img = uint16(reshape(mmc.getImage(), w, h));
         time_map(i, t) = now;
         if t ==1
             EXPOSURE_TMP = EXPOSURE;
             while( min(img(:)) ==0 )
-                EXPOSURE_TMP = EXPOSURE_TMP*0.7 ;
+                EXPOSURE_TMP = EXPOSURE_TMP*0.7;
                 mmc.setExposure(EXPOSURE_TMP);
                 mmc.snapImage();
                 mmc.sleep(EXPOSURE_TMP + 10);
@@ -76,24 +76,23 @@ for i = 1:pos_num
                 img = uint16(reshape(mmc.getImage(), w, h));
                 time_map(i, t) = now;
             end
+            % Write 1 page tiff
             options.overwrite = true;
             options.append = false;
             options.message = false;
             saveastiff(img, fname, options);
         else
+            % Append to tiff
             options.overwrite = false;
             options.append = true;
             options.message = false;
             saveastiff(img, fname, options);
         end
-        t = t + 1;
+        t = t + 1; % Update timer
+        % Wait for next cycle
         while (clock - t_last < t_gap/1000)
-            mmc.sleep(1); % minial fresh time: 1ms
+            mmc.sleep(1); 
         end
-        % Save to stack tiff
-        %options.overwrite=true;
-        %   saveastiff(img, fname, options);
-        %end
     end
 end
 
--- initialize.m
+++ initialize.m
@@ -2,10 +2,13 @@ function mmc = initialize(config)
         import mmcorej.*
         mmc= CMMCore;
         %config = 'config\MMConfig_Ti.cfg';
+        %config = 'C:\users\qblab\Documents\MMConfig_Ti.cfg';
         mmc.loadSystemConfiguration(config);
         mmc.waitForSystem();
         mmc.setTimeoutMs(50000);
-        mmc.enableDebugLog(true);
-        mmc.setPrimaryLogFile('test.log');
-
+        %mmc.enableDebugLog(true);
+        %mmc.setPrimaryLogFile('test.log');
+        mmc.setProperty('Andor sCMOS Camera', 'Sensitivity/DynamicRange', '16-bit (low noise & high well capacity)'); 
+        % Set to 16bit
+        mmc.getProperty('Andor sCMOS Camera', 'AuxiliaryOutSource (TTL I/O)');
 end
