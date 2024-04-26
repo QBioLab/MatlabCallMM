3431580dc78210392e917e844cad5755701d2df8
H.F. <moyuejian@outlook.com>
2024-04-26 14:19:37 +0800

    stitch well plate offline

--- scan_plate/stitch_plate_offline.m
+++ scan_plate/stitch_plate_offline.m
@@ -1,8 +1,8 @@
 clear all
-%metainfo_file = 'E:\XJF\20240422\plate1-sto-gfp-conditioned-medium\plate1-sto-gfp-conditioned-medium.json';
-metainfo_file = 'E:\XJF\20240420\plate1-sto-gfp-conditioned-medium\plate1-sto-gfp-conditioned-medium.json';
-metainfo_json=fileread(metainfo_file);
-metainfo = jsondecode(metainfo_json);
+addpath ../lib
+%metainfo_file = 'H:\XJF\20240420\plate1-sto-gfp-conditioned-medium\plate1-sto-gfp-conditioned-medium.json';
+[metainfo_file, metainfo_dir]=uigetfile('.json');
+metainfo = read_json([metainfo_dir metainfo_file]);
 
 % camera : photometrics kinitex (3200x3200 6.5um)
 nx=metainfo.roi(3); 
@@ -15,15 +15,21 @@ pixel_size=6.5/M/Shrink; %% pixel size in assembled image: 1um
 
 tiff_options.overwrite = true;
 tiff_options.message = false;
-load('E:\analysis_plate_clones\backgrounds\IVS96_center_10XAPO_Kinitex_2720.mat','factors','dark_mean');
-shrinked_flat_field=imresize(factors,Shrink);
-shrinked_dark_field=imresize(dark_mean,Shrink);
+load('H:\analysis_plate_clones\backgrounds\IVS96_center_10XAPO_Kinitex_2720.mat','factors','dark_mean');
+flat_field_pad = ones(ny, nx, 4);
+dark_field_pad = ones(ny, nx).*100;
+flat_field_pad(241:2960, 241:2960, :) = factors;
+dark_field_pad(241:2960, 241:2960) = dark_mean;
+shrinked_flat_field=imresize(flat_field_pad,Shrink);
+shrinked_dark_field=imresize(dark_field_pad,Shrink);
 
 % load shape factor for each image to facilitate merging neighboring images
 % load weight factor for the assembled images with Shrink ratio
 plate.exp=metainfo.sample_name;
 plate.folder_input=metainfo.data_dir;
-plate.folder_out=['E:\analysis_plate_clones\assembled_plates\XJF\20240420\' plate.exp '-test'];
+plate.folder_input(1)='H';
+plate.folder_out=['Y:\analysis_plate_clones\assembled_plates\XJF\20240420\' plate.exp '-subtraced_background'];
+
 plate.nc=length(metainfo.channel_sequence);
 plate.channel_index=metainfo.channel_sequence +1;
 plate.channel = [];
@@ -43,11 +49,12 @@ transformation_matrix = [cosd(plate.rotation) -sind(plate.rotation); ...
                         sind(plate.rotation) cosd(plate.rotation)]/plate.scale;
 
 % calculate the size of the entire well assembly
-view_list=metainfo.well_plate.view([2 3 4 1], :);
+view_list=metainfo.well_plate.view([2 3 4 1], :); % ? why it wroks? coordint direction?
 transformed_view_center=ceil(transformation_matrix* view_list'/pixel_size)';
 min_well_image_pixel=floor(min(transformed_view_center))-nx1/2;
 max_well_image_pixel=ceil(max(transformed_view_center))+(nx1/2);
 well_pixel_sizes=max_well_image_pixel-min_well_image_pixel;
+well_assembled_mask=build_stitching_mask(transformed_view_center, [ny1,nx1], well_pixel_sizes);
 
 for well_count=1:plate.nwell
     well_idx=plate.well_list(well_count);
@@ -57,22 +64,27 @@ for well_count=1:plate.nwell
         view = transformed_view_center(pos_i_perw,:);
         jy=(1:ny1)-ny1/2+view(2)-min_well_image_pixel(:,2);
         jx=(1:nx1)-nx1/2+view(1)-min_well_image_pixel(:,1);
+        well_assembled_unmask=zeros(well_pixel_sizes(2),well_pixel_sizes(1),plate.nc,'uint16');
+        
         for i_ch=1:plate.nc
             tiff_name=sprintf('%s\\ch%d\\well%d_%d_pos%dc%d.tiff', ...
 		    plate.folder_input,i_ch,well_idx, pos_i_perw, pos_id, i_ch);                
             raw_img=loadtiff(tiff_name);
+            
             shrinked_img=imresize(raw_img,Shrink);
-            %flat_field=factors_shrink(:,:,plate.channel_index(i_ch)-1);
-            %flatted_img=(double(shrinked_img)-dark_mean_shrink).*flat_field;
-            %tempy = rolling_ball(temp1,10,30,0.5);
+            %flat_field=shrinked_flat_field(:,:,plate.channel_index(i_ch)-1);
+            %flatted_img=(double(shrinked_img)-shrinked_dark_field).*flat_field;
+            subtracted_background = rolling_ball(shrinked_img,10,30,0.5);
             %flatted_img=uint16(flatted_img);
-            well_assembled(jy,jx,i_ch)=shrinked_img;
+            well_assembled_unmask(jy,jx,i_ch)=subtracted_background;
+            well_assembled = well_assembled + well_assembled_unmask.*well_assembled_mask(:, :, pos_i_perw);
         end
     end 
+
     for i_ch=1:plate.nc
          file_per_well=sprintf('%s\\%s\\well%03d_%s.tif',...
                   plate.folder_out, plate.channel(i_ch), well_idx, plate.channel(i_ch)); 
-       saveastiff(well_assembled(:,:,i_ch),file_per_well,tiff_options);
+         parfeval(@saveastiff, 0, well_assembled(:,:,i_ch), file_per_well, tiff_options);
     end
     %clear well_assembled file_per_well
 end
