d08535f011fa9a043904d4ccf3cd46aced1cc596
H.F. <moyuejian@outlook.com>
2024-06-25 23:53:51 +0800

    saved update

--- stitching/stitching_by_well.m
+++ stitching/stitching_by_well.m
@@ -1,23 +1,38 @@
 function [plate]= stitching_by_well(metainfo)
-%% if all the well share the same layout
+% if all the well share the same layout
+% Used parameters from metainfo:
+%   metainfo.smaple_name
+%   metainfo.roi
+%   metainfo.data_dir
+%   metainfo.active_channel_sequence
+%   metainfo.well_plate.view_list
+%   metainfo.well_plate.picked_well_list
+%   metainfo.camera_affine_matrix
+%   metainfo.chsetup
+
+
 tiff_options.overwrite = true;
 tiff_options.message = true;
 
 %% parse neccessary info from metainfo
 plate.sample=metainfo.sample_name;
 plate.view_dir = metainfo.data_dir;
-plate.assembly_dir = metainfo.well_plate.assembly_dir;
+plate.assembly_dir = metainfo.assembly_dir;
 plate.view_position_list = metainfo.well_plate.view_list;
-plate.view_fname_list = string(metainfo.log.fname_list);
+%plate.view_fname_list = string(metainfo.log.fname_list);
 plate.view_size = [metainfo.roi(3); metainfo.roi(4)]; % ?[dimension x, dimension y]
-plate.channel_index=metainfo.channel_sequence +1;
+plate.channel_index=metainfo.active_channel_sequence;
 plate.well_list = int32(metainfo.well_plate.picked_well_list);
 well_num = length(plate.well_list);
-affine_string = metainfo.camera_affine_matrix;
-camera_affine_matrix = reshape(sscanf(affine_string, '%f;%f;%f;%f;%f;%f'),3,2);
-%plate.affine_matrix = camera_affine_matrix(1:2, 1:2); % scaling & rate, no translate
-plate.affine_matrix = [-0.6459012629133096, -0.00140452112843774;...
-    -0.0014006888550221026, 0.6456566462658601;];
+try 
+    affine_string = metainfo.camera_affine_matrix;
+    camera_affine_matrix = reshape(sscanf(affine_string, '%f;%f;%f;%f;%f;%f'),3,2);
+    plate.affine_matrix = camera_affine_matrix(1:2, 1:2); % scaling & rate, no translate
+    % BUG: translate may required to fit roi setting
+catch 
+    plate.affine_matrix = [-0.6459012629133096, -0.00140452112843774;...
+        -0.0014006888550221026, 0.6456566462658601;];
+end
 
 channel_num = length(plate.channel_index);
 plate.channel = [];
@@ -46,12 +61,13 @@ parfor well_count = 1:well_num
             view_position_in_assembly = view_position_in_camera - min_scan_border +1;
             % plus 1 becuase matlab count from 1
             
-            % if online version: wait image here
+            % New feature: wait image here for online version
             view_fname= sprintf("%s/ch%d/well%d_%d_c%d.tiff", ...
                 plate.view_dir, ch_idx, well_idx, view_idx, ch_idx);
             raw_img=loadtiff(view_fname);
             raw_img = raw_img';
-            %transpose to column-major for matlab;
+            % transpose to column-major for matlab;
+            % New feature: single view processing here
         
             view_range1_in_assembly = view_position_in_assembly(1) : ...
                   view_position_in_assembly(1) + plate.view_size(1)-1;
@@ -62,6 +78,7 @@ parfor well_count = 1:well_num
                 assembly(view_range1_in_assembly, view_range2_in_assembly);
         end
         assembly_fname=sprintf('%s/ch%d/well_%d.tif', plate.assembly_dir, ch_idx, well_idx); 
+        % New feature: shrink here
         saveastiff(assembly, assembly_fname, tiff_options);
     end
 end
